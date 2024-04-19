ae89c2e84c47f3ce38775381e52b1d9729040c6e
Eva <eva.boulanger4@gmail.com>
2024-04-19 15:27:02 +0200

    Etape 3?

---
+++ FrontEnd/evalogin.js
@@ -0,0 +1,8 @@
+// Comment fonctionne la balise Form ?
+// La data est transferee automatiquement a l'API?
+
+
+
+// Je fais quoi avec ma reponse?
+// -> Si oui j'ouvre le mode editeur.
+// -> Si non -> message d'erreur

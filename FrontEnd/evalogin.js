9df8cc90394f488c126f48f495b25ae6d8003155
Eva <eva.boulanger4@gmail.com>
2024-04-26 17:18:47 +0200

    Extraction manuelle terminee

--- FrontEnd/evalogin.js
+++ FrontEnd/evalogin.js
@@ -1,6 +1,7 @@
 // https://fr.javascript.info/formdata
 // L'API accepte seulement  :  'application/json'
 // FormData renvoit du :  'multipart/form-data'
+// let userData = new FormData(anyForm); // Je voudrais recuperer ma data comme ca. et la mettre dans body
 // un FormData en bodyPOST ca ne marche donc pas -> Il faut donc que j'extrais les valeurs des champs moi meme.
 
 
@@ -10,28 +11,40 @@ const loginURL = 'http://localhost:5678/api/users/login'; // L'URL de swagger
 const loginButton = document.querySelector("button"); // Mon bouton "se connecter"
 const loginForm = document.querySelector("form"); // Mon formulaire login
 
+// Je recup les deux champs dans une liste de HTMLElements
+// Je dois assigner la valeur if input.name=password ; let password = input.name.value
+// Je dois ensuite retourner ca dans un format JSON (interpolation)
 
+let extractDataForm = function() { // Return les valeurs du formulaire directement dans le bon format.
+   let passwordInput = document.querySelector('input[name="password"]')
+   let emailInput = document.querySelector('input[name="email"]')
+   return `{
+    "email": "${emailInput.value}",
+    "password": "${passwordInput.value}"
+  }`
+}
 
+// const userData = `{
+//     "email": "sophie.bluel@test.tld",
+//     "password": "S0phie"
+//     }`
 
-
-
-const userData = `{
-    "email": "sophie.bluel@test.tld",
-    "password": "S0phie"
-    }`
+// let bodydata = extractDataForm()
+// console.log(bodydata);
+// console.log(userData)
 
 
 let postFormRequest = async function(anyForm, anyURL) {  // requete POST 
         console.log('Lancement fonction fetch')
     
-        // let userData = new FormData(anyForm); // Je voudrais recuperer ma data comme ca.
+
         console.log(`verification anyForm : ${anyForm}`);
         console.log(`verification anyURL : ${anyURL}`);
 
         let response = await fetch(anyURL, { // requete Post
             method: "POST",
             headers: {"Content-Type": "application/json"},
-            body: userData
+            body: extractDataForm()
         });
 
         let token = await response.json(); // parsing      

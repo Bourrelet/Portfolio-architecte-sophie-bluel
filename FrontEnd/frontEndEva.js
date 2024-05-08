0e1acdb9044d93f3964e053d72518dbc4451aebf
Eva <eva.boulanger4@gmail.com>
2024-05-08 13:50:02 +0200

    Toilettage code

--- FrontEnd/frontEndEva.js
+++ FrontEnd/frontEndEva.js
@@ -1,7 +1,7 @@
 // HOMEPAGE //
+
 // Recuperation DATA
-
-
+// Recuperation DATA
 let getWorks = async function() {
     const response = await fetch('http://localhost:5678/api/works');
     const works = await response.json();
@@ -16,46 +16,12 @@ let getFilters = async function() {
     return filters
 }
 let filters = await getFilters()
+// Recuperation DATA
+// Recuperation DATA
 
-
-
-let delworkUrl = 'http://localhost:5678/api/works/{id}'
-let newWorkUrl = 'http://localhost:5678/api/works'
-
-// Recuperation DATA\  
-
-// Affichage des Filtres
-// DOM Anchor and filter Box
-let folioAnchor = document.querySelector("#portfolio");
-let gallerieGrid = document.querySelector('.gallery')
-let filterBox = document.createElement('div');
-folioAnchor.insertBefore(filterBox, gallerieGrid); // Insertion des filtres juste avant la gallerie 
-
-// Bouton Tous Filter Display
-let tousButton = document.createElement('div'); 
-filterBox.appendChild(tousButton);
-tousButton.innerText = "Tous";
-tousButton.id = 0; 
-tousButton.classList.add("filterButton");
-
-// Dynamic Filters Display
-let displayFilters = function() {
-
-   for(let i = 0; i < filters.length; i++) {
-        let filterTag = document.createElement('div');
-        filterBox.appendChild(filterTag); 
-        filterTag.innerText= filters[i].name;
-        filterTag.id = filters[i].id;   
-        filterTag.classList.add("filterButton");         
-    }
-    filterBox.classList.add("filterBox");
-}
-displayFilters()
-// Affichage des Filtres
-
-// filtrage
+// Display gallerie
+// Display gallerie
 let gallerie = document.querySelector(".gallery");
-
 let displayGallerie = async function() {
     gallerie.innerHTML = "";
     let works = await getWorks();
@@ -67,26 +33,46 @@ let displayGallerie = async function() {
         </figure>`
     }
 };
+window.addEventListener("load",(displayGallerie())); // Au chargement de la page
+// Display gallerie
+// Display gallerie
 
-// Fonctions Filtrage
-let initWork = function() { 
-    console.log('Lancement initWork')
-for(let work of works) { 
-    gallerie.innerHTML += `<figure>
-    <img src="${work.imageUrl}" alt="${work.title}">
-    <figcaption>${work.title}</figcaption>
-    </figure>`
-}}
-window.addEventListener("load",(initWork())); // Au chargement de la page
 
-//Filtrage par click
+// Integration dynamique des filtres
+// Integration dynamique des filtres
+let folioAnchor = document.querySelector("#portfolio");
+let gallerieGrid = document.querySelector('.gallery')
+let filterBox = document.createElement('div');
+folioAnchor.insertBefore(filterBox, gallerieGrid); // Insertion des filtres juste avant la gallerie 
+
+let tousButton = document.createElement('div'); // Bouton Tous Filter Display
+filterBox.appendChild(tousButton);
+tousButton.innerText = "Tous";
+tousButton.id = 0; 
+tousButton.classList.add("filterButton");
+
+let displayFilters = function() { // Dynamic Filters Display
+   for(let i = 0; i < filters.length; i++) {
+        let filterTag = document.createElement('div'); // cree un bouton pour chaque filtre
+        filterBox.appendChild(filterTag); 
+        filterTag.innerText= filters[i].name;
+        filterTag.id = filters[i].id;   // attribue au bouton le meme id que son filtre
+        filterTag.classList.add("filterButton");         
+    }
+    filterBox.classList.add("filterBox");
+}
+displayFilters()
+// Integration dynamique des filtres
+// Integration dynamique des filtres
+
+//Boutons filtres
+//Boutons filtres
 let filtering = function(buttonArg){ // filtre les travaux en fonction du bouton clique
     console.log('filtrage en cours');
     gallerie.innerHTML = ""; // reset gallerie
-    if(buttonArg.id == 0 ){ // tous
-        initWork(); // Affiche toute la gallerie
-        console.log('Lancement initWork');
-    } else {         
+    if(buttonArg.id == 0 ){ // si "tous" est le bouton argument
+        displayGallerie(); // Affiche toute la gallerie
+    } else {        
         for(let work of works) {
             if(work.category.id == buttonArg.id){
                 console.log(`Categorie ${work.category.id} -> ${work.category.name}  button id -> ${buttonArg.id}`)
@@ -94,10 +80,10 @@ let filtering = function(buttonArg){ // filtre les travaux en fonction du bouton
                 <img src="${work.imageUrl}" alt="${work.title}">
                 <figcaption>${work.title}</figcaption>
                 </figure>`
-                    }
                 }
-    }
-}
+            }
+        }
+};
 let buttonList = document.querySelectorAll(".filterButton"); // Identifie le bouton clique et le passe en argument de filtering
 for (let button of buttonList){ // button prend les valeurs de la NodeList
     button.addEventListener("click", () => {
@@ -105,11 +91,14 @@ for (let button of buttonList){ // button prend les valeurs de la NodeList
         filtering(button)
     });
 }
+//Boutons filtres
+//Boutons filtres
 
 
 // HOMEPAGE //
 
-// Changement de page au click de login //
+// Affichage de la page login //
+// Affichage de la page login //
 const homePage = document.querySelector("#homePage");
 const loginPage = document.querySelector("#loginPage");
 const logButt = document.querySelector("#logButton");
@@ -117,43 +106,40 @@ logButt.addEventListener("click", event => {
     homePage.classList.toggle("invisible");
     loginPage.classList.toggle("invisible");    
 })
-// Changement de page au click de login //
+// Affichage de la page login //
+// Affichage de la page login //
 
-
+// Integration Dynamique admin Mod
+// Integration Dynamique admin Mod
 let bodyAnchor = document.querySelector("body");
 let modHeaderBand = document.createElement('header');
-
-// let folioAnchor = document.querySelector("#portfolio");
-let folioTextBox = document.createElement('div') // une boite pour rassembler le text sur une meme ligne
+let folioTextBox = document.createElement('div')
 let folioTitle = document.querySelector('#portfolio h2');
 let modifButton = document.createElement('div');
 
-let adminMod = function() { 
-    // affiche le bandeau noir
-
+let adminMod = function() {  // Je dois encore changer login en logout
     modHeaderBand.innerHTML= `<i class="fa-regular fa-pen-to-square"></i> <p>Mode edition</p>`;
     bodyAnchor.prepend(modHeaderBand); // Definit comme premier-ne de header
+
     modHeaderBand.classList.add("rowBox");
     modHeaderBand.classList.add("modHeaderBand");
-    // Je dois aussi changer login en logout
     
-    // Affiche le bouton modifier
-
-    folioAnchor.prepend(folioTextBox); // La boite en premier enfant de portfolio
+    folioAnchor.prepend(folioTextBox); // La boite txt en premier enfant de portfolio
     folioTextBox.appendChild(folioTitle);
-    folioTextBox.appendChild(modifButton);
+    folioTextBox.appendChild(modifButton); // Affiche le bouton modifier
+
     folioTextBox.classList.add("rowBox");
     modifButton.classList.add("rowBox");
     modifButton.classList.add("modifButton");
     modifButton.classList.add("anyButton");
-   
-    modifButton.innerHTML = `<i class="fa-regular fa-pen-to-square"></i> <p>modifier</p>`
 
-    // Fait disparaitre les filtres
-    filterBox.classList.toggle("invisible");
+    modifButton.innerHTML = `<i class="fa-regular fa-pen-to-square"></i> <p>modifier</p>`
+    filterBox.classList.toggle("invisible");     // Fait disparaitre les filtres
 }
+// Integration Dynamique admin Mod
+// Integration Dynamique admin Mod
+
 let modalePage = document.querySelector("#modalePage");
-modalePage.classList.add("modalePage");
 let modaleBox = document.querySelector(".modaleBox")
 let originalHeader = document.querySelector('header:not([class])');
 let modGallery = document.querySelector(".modGallery")
@@ -163,7 +149,7 @@ let displayModGallery = async function() {
     modGallery.innerHTML = "";
     let works = await getWorks();
     console.log('Lancement displayModGallery')
-    for(let work of works) { // rempli la gallerie de modale1
+    for(let work of works) { // remplit la gallerie de modale1 avec images et corbeilles
         modGallery.innerHTML += `<figure>
         <img src="${work.imageUrl}" alt="${work.title}">
         <div>
